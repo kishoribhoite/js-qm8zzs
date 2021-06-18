993181b67e04460c4220b655e653ce77e38767f3
kishoribhoite <bhoitekishori7@gmail.com>
2021-06-18 22:59:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-qm8zzs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qm8zzs)
---
+++ index.js
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var a = 10;
+var b = "10";
+
+if (a >= b) {
+  document.write('you are good to go');
+} else {
+  document.write('write again');
+}
